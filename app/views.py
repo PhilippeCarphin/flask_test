752f8c46a2b377bc8c3a53f2caa005c9ec889059
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-03-04 20:44:52 -0500

    Added resume

--- app/views.py
+++ app/views.py
@@ -1,9 +1,18 @@
 from app import app
-from flask import render_template
+from flask import render_template, send_from_directory
+
+HOST = 'localhost:5000'
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname':'Phil'}
-    return render_template('index.html', title='Home', user=user,
-            example='example-value', double_curly='{{double-culy}}')
+    return render_template('index.html', host=HOST)
+
+@app.route('/english/resume_en_2018.pdf')
+def resume():
+    return send_from_directory('..','resume_2018_en.pdf')
+
+@app.route('/francais/resume_fr_2018.pdf')
+def resume_fr():
+    return send_from_directory('..','resume_2018_fr.pdf')
