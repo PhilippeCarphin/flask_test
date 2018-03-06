63c5292c3bdc1f24bf903a88d3385cc21be1fe9d
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-03-06 12:20:34 -0500

    Receiving a filename

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, send_from_directory
+from flask import render_template, send_from_directory, request, redirect
 
 HOST = open('app/host.txt').readline()[:-1]
 
@@ -20,3 +20,16 @@ def resume_fr():
 @app.route('/server.jpg')
 def server():
     return send_from_directory('..','server.jpg')
+
+
+@app.route('/sgf-turner', methods=['GET', 'POST'])
+def sgf_turner():
+    if request.method == 'POST':
+
+        if 'sgf-file' not in request.files:
+            print(''''file' not in request.files''')
+            return redirect(request.url)
+        else:
+            print('got filename ' + request.files['sgf-file'].filename)
+
+    return render_template('sgf-turner.html')
