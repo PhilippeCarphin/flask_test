afa5b86e70db3118684bb23a81d5e972b9eb8b9e
Philippe Carphin <phil103@hotmail.com>
2018-05-13 20:39:38 -0400

    Mods

--- sync-and-restart.sh
+++ sync-and-restart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-pushd ~/Documents/GitHub/flask_test
+host_ip=$(echo $SSH_CLIENT | cut -d ' ' -f 1)
 killall flask_env/bin/python
 git fetch
 git reset --hard origin/master
 # Sync up uncommitted things
 pushd app
-rsync -av 'pcarphin@192.168.2.15:~/Documents/GitHub/flask_test/app/*' .
+rsync -av 'pcarphin@'$host_ip':~/Documents/GitHub/flask_test/app/*' .
 popd
 nohup ./run.py &
-
+exit 0
--- update-server.sh
+++ update-server.sh
@@ -1,1 +1,12 @@
-ssh -f pi 'pushd ~/Documents/GitHub/flask_test ; ./sync-and-restart.sh'
+if ! ssh -f pi 'pushd ~/Documents/GitHub/flask_test ; ./sync-and-restart.sh' ;
+then
+    echo 'please make sure host named pi is defined in
+    either ~/.ssh/config as in
+
+        Host pi
+            User pcarphin
+            Port 22
+            Hostname 123.34.56.78
+
+'
+fi
