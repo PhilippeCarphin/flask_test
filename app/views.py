ad9653fc70ebc2fc29e11fa85efc97d70c16d7e0
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-05-22 11:41:26 -0400

    Corrected allowed methods

--- app/views.py
+++ app/views.py
@@ -83,10 +83,10 @@ def protected_route():
         current_user.username,
         current_user.email,
         current_user.password
-    ) + '<form class="form-signin" method="POST" action="/index"><button type="submit">Index</button>'
+    ) + '<form class="form-signin" method="GET" action="/index"><button type="submit">Index</button>'
 
 
-@app.route('/logout', methods=['POST'])
+@app.route('/logout', methods=['GET', 'POST'])
 def logout():
     logout_user()
     return redirect(url_for('apps'))
